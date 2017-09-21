9ba2aa05bf135a504896878a8a1fedeee3bc0722
Shubham shrimali <Shubham.shrimali2015@vit.ac.in>
2017-09-21 21:19:37 +0530

    adding all file

---
+++ js/myscript3.js
@@ -0,0 +1,40 @@
+function formValidation()
+{
+var uid = document.registration.Name;
+var uemail = document.registration.Email;
+if(userid_validation(uid,5,12))
+{
+if(ValidateEmail(uemail))
+{
+}
+}
+return false;
+
+}
+function userid_validation(uid,mx,my)
+{
+var uid_len = uid.value.length;
+if (uid_len == 0 || uid_len >= my || uid_len < mx)
+{
+alert("User Id should not be empty / length be between "+mx+" to "+my);
+uid.focus();
+return false;
+}
+return true;
+}
+
+function ValidateEmail(uemail)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(uemail.value.match(mailformat))
+{
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+uemail.focus();
+return false;
+}
+} 
+}
